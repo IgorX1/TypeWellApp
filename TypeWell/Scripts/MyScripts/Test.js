95729492f89edb379ec3dc39f228902f1166d200
Igor Konobas <kois9911@gmail.com>
2018-09-01 12:38:45 +0300

    keypress event added

--- TypeWell/Scripts/MyScripts/Test.js
+++ TypeWell/Scripts/MyScripts/Test.js
@@ -1,7 +1,19 @@
-﻿function startTest() {
+﻿'use strict';
+var isTestInProcess = false;
+
+//Called when Start Test button is clicked
+function startTest() {
+
+    if (isTestInProcess === true)
+        return;
+    isTestInProcess = true;
+    //NEED:add stop btn activity
+    var textBox = initTextBox('test-text');
+    textBox.addEventListener('keydown', keyboardHandler);
     timerOnGoing();
 }
 
+//Responsible for timing
 function timerOnGoing() {
     var secondsLeft = 60;
     var secondsToShow = secondsLeft;
@@ -10,10 +22,24 @@ function timerOnGoing() {
         else secondsToShow = --secondsLeft;
         document.getElementById('timer').innerHTML = "0:" + secondsToShow;
         if (secondsLeft < 1) {
+            isTestInProcess = false;
             clearInterval(timerId);
             document.getElementById('timer').innerHTML = "1:00";
         }
     }, 1000
     )
+}
+
+//Find text box and make sure that it is empty
+function initTextBox(id) {
+    var textbox = document.getElementById(id);
+    textBox.focus();
+    if (textbox.innerHTML != "")
+        textbox.innerHTML = "";
+    return textbox;
+}
+
+function keyboardHandler(e) {
+    document.getElementById('test-text').innerHTML += String.fromCharCode(e.which);
 }
 
